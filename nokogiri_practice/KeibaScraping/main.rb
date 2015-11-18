3176ad6d98479ea25a9356b4cd68d019aae643e2
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-18 17:19:47 +0900

    @doc refactoring

--- nokogiri_practice/KeibaScraping/main.rb
+++ nokogiri_practice/KeibaScraping/main.rb
@@ -1,7 +1,8 @@
 require './race_scraper.rb'
+require 'pry'
+
 scraper = RaceScraper.new
-scraper.scrape_race('http://keiba.yahoo.co.jp/race/result/1505040911/')
-
 # レースのURLを指定してレースの情報を取得できる
-
+# race = scraper.scrape_race('http://keiba.yahoo.co.jp/race/result/1505040901/')
+ races = scraper.scrape_race_from_list('http://keiba.yahoo.co.jp/race/list/15080409/')
 # その中にある馬の各パラメータから検索して、horce_resultを取得できる
--- nokogiri_practice/KeibaScraping/race_scraper.rb
+++ nokogiri_practice/KeibaScraping/race_scraper.rb
@@ -6,44 +6,55 @@ require './horce.rb'
 
 class RaceScraper
 
+  def scrape_race_from_list(list_url)
+    parsed_doc = get_parsed_doc(list_url)
+    binding.pry
+    nodeset = parsed_doc.css('table.scheLs a')
+    nodeset.each do |node|
+      node.inner_text
+    end
+  end
+
   def scrape_race(race_url)
     # スクレイピングの準備
-    setup_scraping(race_url)
+    parsed_doc = get_parsed_doc(race_url)
+    binding.pry
     # レースオブジェクトを生成
     race = Race.new
     # 場所を取得
-    race_place_desc = @doc.css('#raceTitDay').inner_text
+    race_place_desc = parsed_doc.css('#raceTitDay').inner_text
     race_place_desc.match(/(東京|福島|京都)/) do |md|
-    race.place = md[0]
+      race.place = md[0]
     end
     # 何レース目かを取得
-    race.num = @doc.css('#raceNo').inner_text
+    race.num = parsed_doc.css('#raceNo').inner_text
     # レース名を取得
-    race.name = @doc.css('h1.fntB').inner_text.gsub(/[\n]/,"")
+    race.name = parsed_doc.css('h1.fntB').inner_text.gsub(/[\n]/,"")
     # レース結果を取得
-    race.result = scrape_race_result
+    race.result = scrape_race_result(parsed_doc)
+    binding.pry
+    return race
   end
 
   private
 
-  def setup_scraping(race_url)
-    binding.pry
+  def get_parsed_doc(url)
     #　スクレイピング先のURL
-    url = race_url
+    url = url
     charset = nil
     html = open(url) do |f|
       charset = f.charset # 文字種別を取得
       f.read # htmlを読み込んで変数に渡す
     end
-    # htmlを解析してObjectを作成
-    @doc = Nokogiri::HTML.parse(html,charset)
+    # 解析したhtmlを返す
+    return Nokogiri::HTML.parse(html,charset)
   end
 
-  def scrape_race_result
+  def scrape_race_result(parsed_doc)
     # horceの配列
     horces = []
     # テーブルの列を取得
-    nodeset = @doc.css('table#resultLs tr')
+    nodeset = parsed_doc.css('table#resultLs tr')
     # 最初と最後はヘッダー行なので削除
     nodeset.shift 
     nodeset.pop
@@ -59,7 +70,6 @@ class RaceScraper
       horces.push(horce)
     end
     # resultsを返す
-    p horces
     return horces
   end
 
