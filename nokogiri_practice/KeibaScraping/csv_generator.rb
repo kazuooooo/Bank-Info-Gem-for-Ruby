711cde58b1eae787a974bd0d4b9f0547bab87b3f
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-19 13:57:50 +0900

    csv data output

---
+++ nokogiri_practice/KeibaScraping/csv_generator.rb
@@ -0,0 +1,26 @@
+require 'csv'
+require 'pry'
+
+class CSVGenerator
+  def generate_races_csv(races_data)
+    races_data.each do |race_data|
+      generate_race_csv(race_data)
+    end
+  end
+  def generate_race_csv(race_data)
+    CSV.open("race.csv", "a+"){|csv|
+      csv << [ race_data.place, race_data.num, race_data.name ]
+      csv << [ "着順", "馬番", "名前", "騎手名", "人気", "オッズ" ]
+      race_data.result.each{|horce|
+        csv << [ 
+                 horce.ranking,
+                 horce.number,
+                 horce.name,
+                 horce.jockey,
+                 horce.popularity,
+                 horce.odds
+               ]
+      }
+    }
+  end
+end
--- nokogiri_practice/KeibaScraping/main.rb
+++ nokogiri_practice/KeibaScraping/main.rb
@@ -1,9 +1,11 @@
 require './race_scraper.rb'
+require './csv_generator.rb'
 require 'pry'
+require 'csv'
 
 scraper = RaceScraper.new
-# レースのURLを指定してレースの情報を取得できる
-# race = scraper.scrape_race('http://keiba.yahoo.co.jp/race/result/1505040901/')
- races = scraper.scrape_race_from_list('http://keiba.yahoo.co.jp/race/list/15080409/')
- p races
-# その中にある馬の各パラメータから検索して、horce_resultを取得できる
+csv_generator = CSVGenerator.new
+# Race一覧からRace情報を取得
+races = scraper.scrape_race_from_list('http://keiba.yahoo.co.jp/race/list/15080409/')
+# csvで出力
+csv_generator.generate_races_csv(races)
